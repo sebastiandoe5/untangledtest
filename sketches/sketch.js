b50bb4e474caeea6f87ceeb8b20a4430c3e870f9
sebastiandoe5 <sebastiandoe5@gmail.com>
2019-08-05 13:26:41 +0100

    Sad

--- sketches/sketch.js
+++ sketches/sketch.js
@@ -12,30 +12,63 @@ function setup() {
   bottom = window.innerHeight;
   right = window.innerWidth;
   speed = 5;
-  xdirection = 1;
-  ydirection = 1;
+  xdirection = speed;
+  ydirection = speed;
+  x2direction = speed;
+  y2direction = speed;
+  x3direction = speed;
+  y3direction = speed;
+  x4direction = speed;
+  y4direction = speed;
+  x5direction = speed;
+  y5direction = speed;
+  x6direction = speed;
+  y6direction = speed;
+  x2 = 235;
+  y2 = 976;
+  x3 = 235;
+  y3 = 976;
+  x4 = 235;
+  y4 = 976;
+  x5 = 235;
+  y5 = 976;
+  x6 = 235;
+  y6 = 976;
 }
 
 function draw() {
   // colour the background black
-  background(255);
+  background(78);
   // select white as a colour
   // draw a rectangle
-  rect(x, y, 100, 100);
-  circle(x, 120, 120);
-  if (x > right) {
-    xdirection = xdirection - speed;
+  circle(x +60, y +60, 120);
+  circle(x2 +60, y2 +60, 120);
+  if (x > right - 120) {
+    xdirection = speed * -1;
   };
-  if (y > bottom) {
-    ydirection = ydirection - speed;
+  if (y > bottom - 120) {
+    ydirection = speed * -1;
   };
-  if (x < left) {
-    xdirection = ydirection + speed;
+  if (x < 0) {
+    xdirection = speed;
   };
-  if (y < top) {
-    ydirection = ydirection + speed;
+  if (y < 0) {
+    ydirection = speed;
   };
-  console.log(x,y);
   x += xdirection;
   y += ydirection;
+  if (x2 > right - 120) {
+    x2direction = speed * -1;
+  };
+  if (y2 > bottom - 120) {
+    y2direction = speed * -1;
+  };
+  if (x2 < 0) {
+    x2direction = speed;
+  };
+  if (y2 < 0) {
+    y2direction = speed;
+  };
+  x2 += x2direction;
+  y2 += y2direction;
 }
